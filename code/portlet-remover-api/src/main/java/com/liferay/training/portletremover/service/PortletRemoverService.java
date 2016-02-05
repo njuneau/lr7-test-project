889a7d0c9f62d2dd07d09b2ff75c78fe8b056d07
Nicolas Juneau <nicolas.juneau@savoirfairelinux.com>
2016-02-05 11:56:27 -0500

    Added API documentation

--- code/portlet-remover-api/src/main/java/com/liferay/training/portletremover/service/PortletRemoverService.java
+++ code/portlet-remover-api/src/main/java/com/liferay/training/portletremover/service/PortletRemoverService.java
@@ -7,12 +7,27 @@ import com.liferay.portal.model.Layout;
 import com.liferay.portal.model.Portlet;
 
 /**
- * This service is used to detect the portlets in a given layout
+ * This service is used to detect the portlets in a given layout and remove them
+ * if needed.
  */
 public interface PortletRemoverService {
 
+    /**
+     * Returns a list of portlets that are installed on the given layout
+     *
+     * @param layout The layout on which to detect portlets
+     * @return A list of portlets that are installed on the given layout
+     */
     public List<Portlet> getPortlets(Layout layout);
 
+    /**
+     * Removes the given portlet from the given layout
+     *
+     * @param layout The layout on which to remove the portlet
+     * @param portletId The portlet ID to remove
+     * @return True if the portlet was removed, false otherwize
+     * @throws PortalException Thrown if something goes wrong during deletion
+     */
     public boolean removePortlet(Layout layout, String portletId) throws PortalException;
 
 }
